98816e0aad611d178f70299c4cd661b19679248b
Christian Thindberg <christian@thindberg.no>
2015-12-13 21:28:46 +0100

    updated sample.properties

--- samples/basic_sample/consumer/sample_kcl_app.js
+++ samples/basic_sample/consumer/sample_kcl_app.js
@@ -22,6 +22,7 @@ var util = require('util');
 var kcl = require('../../..');
 var logger = require('../../util/logger');
 
+
 // CT own additions
 var redis = require("redis");
 var os = require("os");
@@ -64,11 +65,16 @@ function recordProcessor() {
       var record, data, sequenceNumber, partitionKey;
       for (var i = 0 ; i < records.length ; ++i) {
         record = records[i];
-        data = new Buffer(record.data, 'base64').toString();
+        data = new Buffer(record.data, 'base64'); //.toString();
+        var obj = JSON.parse(data);
         sequenceNumber = record.sequenceNumber;
         partitionKey = record.partitionKey;
         // TODO: read topic from the record and publish to redis-channel accordingly
-        redisClient.publish ("InfrastructureStationsChannel", data);
+        log.info("object: " + JSON.stringify(obj));
+        log.info("data.topic:" + obj["topic"]);
+        log.info("frequency: " + obj["frequency"]);
+        log.info("values: " + obj["values"]);
+        redisClient.publish (obj["topic"], data);
         log.info(util.format('ShardID: %s, Record: %s, SeqenceNumber: %s, PartitionKey:%s', shardId, data, sequenceNumber, partitionKey));
       }
       if (!sequenceNumber) {
